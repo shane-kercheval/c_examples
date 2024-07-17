66c12662959da35b15367aeacde992319b4f29b4
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-07-16 20:56:15 -0700

    updated cli to take multiple arguments (num producers, consumers, buffer size)

--- c_examples/producer_consumer/src/main.c
+++ c_examples/producer_consumer/src/main.c
@@ -14,19 +14,34 @@
 
 
 int main(int argc, char *argv[]) {
-    // printf("Num args: %d\n", argc);
-    // for (int i = 0; i < argc; i++) {
-    //     printf("Arg %d: %s\n", i, argv[i]);
-    // }
+    // first argument is the number of producer threads
+    // second argument is the number of consumer threads
+    // third argument is the size of the buffer
     printf("\n\nRelease Mode (NDEBUG DEFINED): %s\n\n", NDEBUG_DEFINED ? "true" : "false");
-    printf("hello\n");
-    struct timeval start, end;
-    gettimeofday(&start, NULL);
-    sleep(1);
-    gettimeofday(&end, NULL);
-    long elapsed = duration(start, end);
-    printf("Elapsed time: %ld microseconds\n", elapsed);
-    printf("goodbye\n");
+    
+    int num_args = argc - 1;
+    if (num_args != 3) {
+        printf("Usage: %s <num_producers> <num_consumers> <buffer_size>\n", argv[0]);
+        return 1;
+    }    
+    int num_producers = atoi(argv[1]);
+    int num_consumers = atoi(argv[2]);
+    int buffer_size = atoi(argv[3]);
+    printf("num_producers: %d\n", num_producers);
+    printf("num_consumers: %d\n", num_consumers);
+    printf("buffer_size: %d\n", buffer_size);
+    
+    
+    
+    
+    // printf("hello\n");
+    // struct timeval start, end;
+    // gettimeofday(&start, NULL);
+    // sleep(1);
+    // gettimeofday(&end, NULL);
+    // long elapsed = duration(start, end);
+    // printf("Elapsed time: %ld microseconds\n", elapsed);
+    // printf("goodbye\n");
 
     return 0;
 }
