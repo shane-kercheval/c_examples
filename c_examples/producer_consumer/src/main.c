af68ce2d86c3f09db312b1f2b568e7be5db36a60
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-07-17 08:17:57 -0700

    added initial thread/buffer variables

--- c_examples/producer_consumer/src/main.c
+++ c_examples/producer_consumer/src/main.c
@@ -2,8 +2,9 @@
 #include <unistd.h>
 // #include <stdlib.h>
 #include <stdbool.h>
-#include <assert.h>
+#include <pthread.h>
 #include "utils.h"
+#include "buffer.h"
 
 // NDEBUG is defined in Release mode
 #ifdef NDEBUG
@@ -17,7 +18,7 @@ int main(int argc, char *argv[]) {
     // first argument is the number of producer threads
     // second argument is the number of consumer threads
     // third argument is the size of the buffer
-    printf("\n\nRelease Mode (NDEBUG DEFINED): %s\n\n", NDEBUG_DEFINED ? "true" : "false");
+    printf("\n\nRelease Mode (NDEBUG DEFINED): %s\n\n", NDEBUG_DEFINED ? "true-Release" : "false-Debug");
     
     int num_args = argc - 1;
     if (num_args != 3) {
@@ -31,8 +32,13 @@ int main(int argc, char *argv[]) {
     printf("num_consumers: %d\n", num_consumers);
     printf("buffer_size: %d\n", buffer_size);
     
-    
-    
+    pthread_t *producer_threads = (pthread_t *)malloc_or_die(num_producers * sizeof(pthread_t));
+    pthread_t *consumer_threads = (pthread_t *)malloc_or_die(num_consumers * sizeof(pthread_t));
+
+    // need to define this globally or pass it as an argument to the producer and consumer functions
+    // so it would need to be defined on the heap
+    Buffer *buffer = create_buffer(buffer_size);
+
     
     // printf("hello\n");
     // struct timeval start, end;
@@ -43,5 +49,8 @@ int main(int argc, char *argv[]) {
     // printf("Elapsed time: %ld microseconds\n", elapsed);
     // printf("goodbye\n");
 
+    free(producer_threads);
+    free(consumer_threads);
+    destroy_buffer(buffer);
     return 0;
 }
