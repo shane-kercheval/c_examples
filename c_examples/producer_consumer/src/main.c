7fe5c901bad3551983a0e0f788c35a4aa4d597a2
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-07-18 09:03:13 -0700

    fixed memory issue where we needed to clean up arguments allocated and sent to threads

--- c_examples/producer_consumer/src/main.c
+++ c_examples/producer_consumer/src/main.c
@@ -16,7 +16,7 @@
     bool NDEBUG_DEFINED = false;
 #endif
 
-int total_requests = 0;
+volatile int total_requests = 0; // need volatile to prevent compiler optimizations
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 // OSTEP: "producer threads wait on the condition `not_full`, and signals `not_empty`"
 // OSTEP: "consumer threads wait on the condition `not_empty`, and signals `not_full`"
@@ -38,7 +38,8 @@ void* producer(void* arg) {
     Buffer* buffer = args->buffer;
     int thread_id = args->thread_id;
     int max_requests = args->max_requests;
-
+    printf("Producer %d started\n", thread_id);
+    fflush(stdout);
     // generate fixed number of requests
     // while (total_requests < max_requests) {   // this is causing deadlock because total_requests is not locked
     while (true) {
@@ -83,6 +84,8 @@ void* consumer(void* arg) {
     Buffer* buffer = args->buffer;
     int thread_id = args->thread_id;
     int max_requests = args->max_requests;
+    printf("Consumer %d started\n", thread_id);
+    fflush(stdout);
 
     while (true) {
         MUTEX_LOCK(&mutex);
@@ -121,27 +124,31 @@ int main(int argc, char* argv[]) {
     int buffer_size = atoi(argv[3]);
     int max_requests = atoi(argv[4]);
     
+    // I need to pass in pointers to the thread functions, so I need to allocate memory for each thread's arguments
+    // and store the pointers in an array so I can free them later
+    ThreadArgs** args_ptrs = (ThreadArgs**)malloc_or_die((num_producers + num_consumers) * sizeof(ThreadArgs*));
     pthread_t* producer_threads = (pthread_t*)malloc_or_die(num_producers * sizeof(pthread_t));
     pthread_t* consumer_threads = (pthread_t*)malloc_or_die(num_consumers * sizeof(pthread_t));
     Buffer* buffer_ptr = create_buffer(buffer_size);
 
-    printf("Running producer-consumer simulation with %d producers, %d consumers, buffer size %d, max requests %d\n", num_producers, num_consumers, buffer_size, max_requests);
-    
+    printf("\n---\nRunning producer-consumer simulation with %d producers, %d consumers, buffer size %d, max requests %d\n", num_producers, num_consumers, buffer_size, max_requests);
     struct timeval start, end;
     gettimeofday(&start, NULL);
     for (int i = 0; i < num_producers; i++) {
-        ThreadArgs* args = (ThreadArgs*)malloc_or_die(sizeof(ThreadArgs));
-        args->thread_id = i;
-        args->max_requests = max_requests;
-        args->buffer = buffer_ptr;
-        PTHREAD_CREATE(&producer_threads[i], producer, (void*)args);
+        args_ptrs[i] = (ThreadArgs*)malloc_or_die(sizeof(ThreadArgs));;
+        args_ptrs[i]->thread_id = i;
+        args_ptrs[i]->max_requests = max_requests;
+        args_ptrs[i]->buffer = buffer_ptr;
+        printf("Creating producer %d\n", i);
+        PTHREAD_CREATE(&producer_threads[i], producer, (void*)args_ptrs[i]);
     }
     for (int i = 0; i < num_consumers; i++) {
-        ThreadArgs* args = (ThreadArgs*)malloc_or_die(sizeof(ThreadArgs));
-        args->thread_id = i;
-        args->max_requests = max_requests;
-        args->buffer = buffer_ptr;
-        PTHREAD_CREATE(&consumer_threads[i], consumer, (void*)args);
+        args_ptrs[num_producers + i] = (ThreadArgs*)malloc_or_die(sizeof(ThreadArgs));;
+        args_ptrs[i]->thread_id = i;
+        args_ptrs[i]->max_requests = max_requests;
+        args_ptrs[i]->buffer = buffer_ptr;
+        printf("Creating producer %d\n", i);
+        PTHREAD_CREATE(&consumer_threads[i], consumer, (void*)args_ptrs[i]);
     }
     for (int i = 0; i < num_producers; i++) {
         PTHREAD_JOIN(producer_threads[i]);
@@ -152,13 +159,25 @@ int main(int argc, char* argv[]) {
     gettimeofday(&end, NULL);
     long elapsed = duration(start, end);
 
+    printf("\n---\nRunning in %s\n\n", NDEBUG_DEFINED ? "Release (NDEBUG defined; e.g. assertion statements removed)" : "Debug (NDEBUG not defined; e.g. assertion statements included)");
     printf("num_producers: %d\n", num_producers);
     printf("num_consumers: %d\n", num_consumers);
     printf("buffer_size: %d\n", buffer_size);
     printf("max_requests: %d\n", max_requests);
+    printf("Final Requests Generated: %d\n", total_requests);
+    if (total_requests != max_requests) {
+        fprintf(stderr, "\n**Error**: total_requests != max_requests\n\n");
+    }
+    printf("Final Buffer Count: %d\n", buffer_ptr->count);
+    if (buffer_ptr->count != 0) {
+        fprintf(stderr, "\n**Error**: buffer_ptr->count != 0\n\n");
+    }
     printf("Elapsed time: %ld microseconds\n", elapsed);
-    printf("\nRunning in %s\n\n", NDEBUG_DEFINED ? "Release (NDEBUG defined; e.g. assertion statements removed)" : "Debug (NDEBUG not defined; e.g. assertion statements included)");
     
+    for (int i = 0; i < num_producers + num_consumers; i++) {
+        free(args_ptrs[i]);
+    }
+    free(args_ptrs);
     free(producer_threads);
     free(consumer_threads);
     destroy_buffer(buffer_ptr);
