9c4c9e038054df9675ff1f5aab2309a96e521a0f
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-07-17 20:00:42 -0700

    added consumer/producer functions/threads

--- c_examples/producer_consumer/src/main.c
+++ c_examples/producer_consumer/src/main.c
@@ -1,3 +1,6 @@
+/*
+* Some of this code has been inspired by and/or modified from `Operating Systems: Three Easy Pieces (p. 383).`
+*/
 #include <stdio.h>
 #include <unistd.h>
 // #include <stdlib.h>
@@ -6,51 +9,116 @@
 #include "utils.h"
 #include "buffer.h"
 
-// NDEBUG is defined in Release mode
+// NDEBUG (No Debug) is defined in Release mode and removes assertion statements
 #ifdef NDEBUG
     bool NDEBUG_DEFINED = true;
 #else
     bool NDEBUG_DEFINED = false;
 #endif
 
+int total_requests = 0;
+const int MAX_REQUESTS = 100;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+// OSTEP: "producer threads wait on the condition `not_full`, and signals `not_empty`"
+// OSTEP: "consumer threads wait on the condition `not_empty`, and signals `not_full`"
+pthread_cond_t not_full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t not_empty = PTHREAD_COND_INITIALIZER;
 
-int main(int argc, char *argv[]) {
-    // first argument is the number of producer threads
-    // second argument is the number of consumer threads
-    // third argument is the size of the buffer
-    printf("\n\nRelease Mode (NDEBUG DEFINED): %s\n\n", NDEBUG_DEFINED ? "true-Release" : "false-Debug");
-    
+/*
+ * Worker thread function that produces requests.
+ * Required format:  void *(*start_routine)(void *)
+*/
+void* producer(void* arg) {
+    Buffer* buffer = (Buffer*)arg;
+    // generate fixed number of requests
+    while (total_requests < MAX_REQUESTS) {
+        MUTEX_LOCK(&mutex);
+        while (buffer->count == buffer->size) {
+            // When the count is equal to the size of the buffer, the buffer is full.
+            // We are going to wait until the not_full condition is signaled.
+            // We pass in mutex which will be unlocked while waiting and re-locked when signaled.
+            COND_WAIT(&not_full, &mutex);
+        }
+        HttpRequest request;
+        request.request_id = total_requests;
+        sprintf(request.data, "%d", total_requests);
+        put(buffer, request);
+        total_requests++;
+        // Signal the not_empty condition to wake up any waiting consumers.
+        COND_SIGNAL(&not_empty);
+        MUTEX_UNLOCK(&mutex);
+        printf("Producer producing request %d: %s\n", request.request_id, request.data);
+    }
+    return NULL;
+}
+
+/*
+* Worker thread function that consumes requests.
+* Required format:  void *(*start_routine)(void *)
+*/
+void* consumer(void* arg) {
+    Buffer* buffer = (Buffer*)arg;
+    while (buffer->count != 0 || total_requests < MAX_REQUESTS) {
+        MUTEX_LOCK(&mutex);
+        while (buffer->count == 0) {
+            // When the count is 0, the buffer is empty.
+            // We are going to wait until the not_empty condition is signaled.
+            // We pass in mutex which will be unlocked while waiting and re-locked when signaled.
+            COND_WAIT(&not_empty, &mutex);
+        }
+        HttpRequest request = get(buffer);
+        // Signal the not_full condition to wake up any waiting producers.
+        COND_SIGNAL(&not_full);
+        MUTEX_UNLOCK(&mutex);
+        // Simulate request processing
+        printf("Consumer processing request %d: %s\n", request.request_id, request.data);
+        // sleep(1);
+    }
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
     int num_args = argc - 1;
     if (num_args != 3) {
-        printf("Usage: %s <num_producers> <num_consumers> <buffer_size>\n", argv[0]);
+        printf("Usage: <num_producers> <num_consumers> <buffer_size>\n");
         return 1;
     }    
     int num_producers = atoi(argv[1]);
     int num_consumers = atoi(argv[2]);
     int buffer_size = atoi(argv[3]);
+    
+    pthread_t* producer_threads = (pthread_t*)malloc_or_die(num_producers * sizeof(pthread_t));
+    pthread_t* consumer_threads = (pthread_t*)malloc_or_die(num_consumers * sizeof(pthread_t));
+    Buffer* buffer_ptr = create_buffer(buffer_size);
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < num_producers; i++) {
+        PTHREAD_CREATE(&producer_threads[i], producer, (void*)buffer_ptr);
+    }
+    for (int i = 0; i < num_consumers; i++) {
+        PTHREAD_CREATE(&consumer_threads[i], consumer, (void*)buffer_ptr);
+    }
+    for (int i = 0; i < num_producers; i++) {
+        PTHREAD_JOIN(producer_threads[i]);
+    }
+    for (int i = 0; i < num_consumers; i++) {
+        PTHREAD_JOIN(consumer_threads[i]);
+    }
+
+    gettimeofday(&end, NULL);
+    long elapsed = duration(start, end);
+    printf("Elapsed time: %ld microseconds\n", elapsed);
+
     printf("num_producers: %d\n", num_producers);
     printf("num_consumers: %d\n", num_consumers);
     printf("buffer_size: %d\n", buffer_size);
+    printf("\nRunning in %s\n\n", NDEBUG_DEFINED ? "Release (NDEBUG defined; e.g. assertion statements removed)" : "Debug (NDEBUG not defined; e.g. assertion statements included)");
     
-    pthread_t *producer_threads = (pthread_t *)malloc_or_die(num_producers * sizeof(pthread_t));
-    pthread_t *consumer_threads = (pthread_t *)malloc_or_die(num_consumers * sizeof(pthread_t));
-
-    // need to define this globally or pass it as an argument to the producer and consumer functions
-    // so it would need to be defined on the heap
-    Buffer *buffer = create_buffer(buffer_size);
-
-    
-    // printf("hello\n");
-    // struct timeval start, end;
-    // gettimeofday(&start, NULL);
-    // sleep(1);
-    // gettimeofday(&end, NULL);
-    // long elapsed = duration(start, end);
-    // printf("Elapsed time: %ld microseconds\n", elapsed);
-    // printf("goodbye\n");
-
     free(producer_threads);
     free(consumer_threads);
-    destroy_buffer(buffer);
+    destroy_buffer(buffer_ptr);
+    MUTEX_DESTROY(&mutex);
     return 0;
 }
--- c_examples/producer_consumer/src/utils.h
+++ c_examples/producer_consumer/src/utils.h
@@ -11,7 +11,16 @@
  * Wrapper around functions that need to check for NULL.
  * `assert` statements will be removed if NDEBUG is defined (i.e. built in Release mode).
  */
-// #define MALLOC(size) ({ void *ptr = malloc(size); assert(ptr != NULL); ptr; })
+#define PTHREAD_CREATE(thread, start_routine, arg) assert(pthread_create(thread, NULL, start_routine, arg) == 0);
+#define PTHREAD_JOIN(thread) assert(pthread_join(thread, NULL) == 0);
+
+#define MUTEX_LOCK(mutex) assert(pthread_mutex_lock(mutex) == 0);
+#define MUTEX_UNLOCK(mutex) assert(pthread_mutex_unlock(mutex) == 0);
+#define MUTEX_DESTROY(mutex) assert(pthread_mutex_destroy(mutex) == 0);
+
+#define COND_SIGNAL(cond) assert(pthread_cond_signal(cond) == 0);
+#define COND_WAIT(cond, mutex) assert(pthread_cond_wait(cond, mutex) == 0);
+
 
 /*
  * Calculate the duration between two timevals in microseconds.
