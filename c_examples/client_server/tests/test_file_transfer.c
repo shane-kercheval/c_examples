63f5401381bcf7f4a54ed4458c5f278eb0a3d099
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-07-21 13:45:13 -0700

    file transfer unit tests

--- c_examples/client_server/tests/test_file_transfer.c
+++ c_examples/client_server/tests/test_file_transfer.c
@@ -10,12 +10,19 @@ void test__request_file_metadata__no_server_listening() {
     TEST_ASSERT_EQUAL_INT(ERROR_SEND_FAILED, status);
 }
 
-// void test__send_file_metadata__no_client_listening() {
-//     int socket = 0;
-//     const char* file_name = "test.txt";
-//     int status = send_file_metadata(socket, file_name);
-//     TEST_ASSERT_EQUAL_INT(ERROR_SEND_FAILED, status);
-// }
+void test__send_file_metadata__file_not_exists() {
+    int socket = 0;
+    const char* file_name = "this_file_does_not_exist.txt";
+    int status = send_file_metadata(socket, file_name);
+    TEST_ASSERT_EQUAL_INT(ERROR_FILE_NOT_FOUND, status);
+}
+
+void test__send_file_metadata__no_client_listening() {
+    int socket = 0;
+    const char* file_name = "test.txt";
+    int status = send_file_metadata(socket, file_name);
+    TEST_ASSERT_EQUAL_INT(ERROR_SEND_FAILED, status);
+}
 
 void setUp(void) {}
 void tearDown(void) {}
@@ -23,5 +30,7 @@ void tearDown(void) {}
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test__request_file_metadata__no_server_listening);
+    RUN_TEST(test__send_file_metadata__file_not_exists);
+    RUN_TEST(test__send_file_metadata__no_client_listening);
     return UNITY_END();
 }
