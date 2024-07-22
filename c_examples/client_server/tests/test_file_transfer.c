0c758682bb00962737751fcb0e1a9250965760f5
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-07-21 23:15:48 -0700

    Update test_file_transfer.c

--- c_examples/client_server/tests/test_file_transfer.c
+++ c_examples/client_server/tests/test_file_transfer.c
@@ -36,6 +36,28 @@ void* server_worker(void* arg) {
     return NULL;
 }
 
+void test__request_file_metadata__no_server_listening() {
+    int socket = 0;
+    const char* file_name = "test.txt";
+    Response response;
+    int status = request_file_metadata(socket, file_name, &response);
+    TEST_ASSERT_EQUAL_INT(ERROR_SEND_FAILED, status);
+}
+
+void test__send_file_metadata__file_not_exists() {
+    int socket = 0;
+    const char* file_name = "this_file_does_not_exist.txt";
+    int status = send_file_metadata(socket, file_name);
+    TEST_ASSERT_EQUAL_INT(ERROR_FILE_NOT_FOUND, status);
+}
+
+void test__send_file_metadata__no_client_listening() {
+    int socket = 0;
+    const char* file_name = "test.txt";
+    int status = send_file_metadata(socket, file_name);
+    TEST_ASSERT_EQUAL_INT(ERROR_SEND_FAILED, status);
+}
+
 void test__request_file_metadata__success() {
     pthread_t server_thread;
     int status = pthread_create(&server_thread, NULL, server_worker, NULL);
@@ -63,28 +85,6 @@ void test__request_file_metadata__success() {
     // check that the payload is correctly parsed/returned in the response
     TEST_ASSERT_TRUE(memcmp(response.payload, expected_metadata, expected_payload_size) == 0);
     destroy_response(&response);
-}
-
-void test__request_file_metadata__no_server_listening() {
-    int socket = 0;
-    const char* file_name = "test.txt";
-    Response response;
-    int status = request_file_metadata(socket, file_name, &response);
-    TEST_ASSERT_EQUAL_INT(ERROR_SEND_FAILED, status);
-}
-
-void test__send_file_metadata__file_not_exists() {
-    int socket = 0;
-    const char* file_name = "this_file_does_not_exist.txt";
-    int status = send_file_metadata(socket, file_name);
-    TEST_ASSERT_EQUAL_INT(ERROR_FILE_NOT_FOUND, status);
-}
-
-void test__send_file_metadata__no_client_listening() {
-    int socket = 0;
-    const char* file_name = "test.txt";
-    int status = send_file_metadata(socket, file_name);
-    TEST_ASSERT_EQUAL_INT(ERROR_SEND_FAILED, status);
 }
 
 void setUp(void) {}
