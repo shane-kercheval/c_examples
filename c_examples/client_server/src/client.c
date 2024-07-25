ba2bca420667c73dd91d36ad5192b78c1d050139
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-07-24 18:04:40 -0700

    simple client and server

--- c_examples/client_server/src/client.c
+++ c_examples/client_server/src/client.c
@@ -1,11 +1,58 @@
+#include "sockets.h"
+#include "protocol.h"
+#include "file_transfer.h"
 #include "utils.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void client_function() {
-    printf("Client function called.\n");
-}
+#define PORT 9002
+#define ADDRESS "0.0.0.0"
 
-int main() {
-    client_function();
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <command> <file_name>\n", argv[0]);
+        return 1;
+    }
+    int command = atoi(argv[1]);
+    const char* file_name = argv[2];
+    
+    int server_socket;
+    int status;
+    Response response;
+    switch (command) {
+        case 0:
+            printf("\n\nRequesting File Metadata: `%s`\n", file_name);
+            server_socket = connect_with_retry_or_die(ADDRESS, PORT, 3, 1);
+            status = request_file_metadata(server_socket, file_name, &response);
+            socket_cleanup(server_socket);
+            if (status != STATUS_OK) {
+                printf("Error requesting file metadata: %d, %d\n", status, response.header.error_code);
+                printf("Error message: %s\n", (char*) response.payload);
+                return 1;
+            }
+            printf("Received metadata for file `%s` - `%s`\n\n", file_name, (char*) response.payload);
+            destroy_response(&response);
+            break;
+        case 1:
+            printf("\n\nRequesting File Contents: `%s`\n", file_name);
+            server_socket = connect_with_retry_or_die(ADDRESS, PORT, 3, 1);
+            status = request_file_contents(server_socket, file_name, &response);
+            socket_cleanup(server_socket);
+            if (status != STATUS_OK) {
+                printf("Error requesting file contents: %d, %d\n", status, response.header.error_code);
+                printf("Error message: %s\n", (char*) response.payload);
+                return 1;
+            }
+            printf("Payload size: %d\n", response.header.payload_size);
+            printf("Number of chunks: %d\n", response.header.chunk_index + 1);
+            printf("Received contents for file `%s`:\n", file_name);
+            printf("Contents:\n------\n%s\n------\n\n", (char*) response.payload);
+            destroy_response(&response);
+            break;
+            break;
+        default:
+            printf("Unknown command\n");
+            break;
+    }
     return 0;
 }
--- c_examples/client_server/src/server.c
+++ c_examples/client_server/src/server.c
@@ -1,18 +1,66 @@
 #include "utils.h"
+#include "protocol.h"
+#include "file_transfer.h"
+#include <sockets.h>
+#include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <stdio.h>
 
-void server_function() {
-    printf("Server function called.\n");
+#define PORT 9002
+
+/**
+ * @brief accept a connection and return the client socket, or -1 if the connection fails.
+ */
+int accept_connection(int server_socket) {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    return accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
 }
 
 int main() {
-    server_function();
-    // int response = utils_function();
-    // printf("Utils function returned %d.\n", response);
-    char message[255];
-    snprintf(message, sizeof(message), "Hello, World!");
-    printf("`%s`\n", message);
-    printf("Message length: %d\n", (int)strlen(message));
+    printf("\n\nServer started\n");
+    int server_socket = bind_or_die(PORT);
+    printf("Server bound to port %d\n", PORT);
+    listen_or_die(server_socket, 1);
+    while (1) {
+        printf("\n---------\nWaiting for connection\n");
+        int client_socket = accept_connection(server_socket);
+        if (client_socket == -1) {
+            fprintf(stderr, "***ERROR*** accepting connection\n");
+            socket_cleanup(client_socket);
+            continue;
+        }
+        printf("Connection accepted\n");
+        uint8_t buffer[MAX_MESSAGE_SIZE];
+        ssize_t bytes_received = recv(client_socket, buffer, MAX_MESSAGE_SIZE, 0);
+        if (bytes_received <= 0) {
+            fprintf(stderr, "***ERROR*** receiving message\n");
+            socket_cleanup(client_socket);
+            continue;
+        }
+        Response response;
+        int status = parse_message(buffer, bytes_received, &response);
+        if (status != STATUS_OK) {
+            fprintf(stderr, "Error parsing message\n");
+            socket_cleanup(client_socket);
+            continue;
+        }
+        printf("Received request: command=%d, payload=%s\n", response.header.command, (char*)response.payload);
+        status = handle_request(client_socket, &response.header, response.payload);
+        if (status != STATUS_OK) {
+            fprintf(stderr, "Error handling request: status=%d\n", status);
+            socket_cleanup(client_socket);
+            continue;
+        }
+        else {
+            printf("Request handled\n");
+        
+        }
+        socket_cleanup(client_socket);
+        destroy_response(&response);
+    }
+    socket_cleanup(server_socket);
     return 0;
 }
